f0f14618a20838fc717ea4928616bf296f62c311
Ivan Duran <ivan.duran@indenext.com>
2016-04-06 22:15:41 +0200

    Version 1.0

--- OpenlibraryExample/ViewController.swift
+++ OpenlibraryExample/ViewController.swift
@@ -6,20 +6,75 @@
 //  Copyright © 2016 Indenext. All rights reserved.
 //
 
+// Coursera. Curso: "Accediendo a la nube con iOS". Instituto tecnológico de Monterrey.
+// Tarea Semana 1. Petición al servidor openlibrary.org
+
+
+//Instrucciones
+//
+//En este entregable desarrollarás una aplicación usando Xcode que realice una petición a https://openlibrary.org/
+//
+//Para ello deberás crear una interfaz de usuario, usando la herramienta Storyboard que contenga:
+//
+//Una caja de texto para capturar el ISBN del libro a buscar
+//EL botón de "enter" del teclado del dispositivo deberá ser del tipo de búsqueda ("Search")
+//El botón de limpiar ("clear") deberá estar siempre presente
+//Una vista texto (Text View) para mostrar el resultado de la petición
+//Un ejemplo de URL para acceder a un libro es:
+//
+// https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:978-84-376-0494-7
+//
+//Su programa deberá sustituir el último código de la URL anterior (en este caso 978-84-376-0494-7) por lo que se ponga en la caja de texto
+//
+//Al momento de presionar buscar en el teclado, se deberá mostrar los datos crudos (sin procesar) producto de la consulta en la vista texto en concordancia con el ISBN que se ingreso en la caja de texto
+//
+//En caso de error (problemas con Internet), se deberá mostrar una alerta indicando esta situación
+//
+//Sube tu solución a GitHub e ingresa la URL en el campo correspondiente
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    let baseURL = "https://openlibrary.org"
+    let responseFormat = "json"
 
+    @IBOutlet weak var isbnTextfield: UITextField!
+    @IBOutlet weak var resultTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func search(){
+        
+        let urlStr = baseURL + "/api/books?jscmd=data&format=" + responseFormat + "&bibkeys=" + isbnTextfield.text!
+        
+        // En caso de error, informar al usuario.
+        if let url = NSURL(string: urlStr), let datos:NSData? = NSData(contentsOfURL: url){
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            resultTextView.text = (texto! as String)
+        }else{
+            resultTextView.text = "Parece que hay algún problema con la conexión."
+        }
+    }
+    
+    @IBAction func textFieldReturn(sender: AnyObject) {
+        self.resignFirstResponder()
+        
+        guard isbnTextfield.text!.characters.count > 0 else {
+            return
+        }
+        search()
+    }
 }
 
